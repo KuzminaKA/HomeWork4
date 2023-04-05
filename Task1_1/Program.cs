72d22ff49d3df3a870462566ee2425503222ed4b
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-05 15:58:44 +0300

    Task2 added

---
+++ Task1_1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Read(string number) 
+
+{     
+    System.Console.WriteLine($"Введите число {number}:");
+    int x = int.Parse(Console.ReadLine());     
+    return x; 
+}  
+
+int A=Read("A");
+int B=Read("B");
+
+int Degree1(int A, int B)
+{
+    int x=A;
+    for (int i = 2; i <= B; i++)
+    {
+        x=x*A;
+    }
+       return x; 
+    }
+    System.Console.WriteLine($"A в степени B= {Degree1(A,B)}");
