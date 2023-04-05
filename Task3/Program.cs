315ec683c1d8fd4e89d38753dc8e9a77a063bd3d
KseniaKuzmina <ksenya.kuzmina83@gmail.com>
2023-04-05 15:59:57 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] array=new int[8];
+System.Console.Write("[ ");
+for (int i = 0; i < array.Length; i++)
+{
+   array [i] = new Random().Next(0,100);
+   System.Console.Write(" ");
+    System.Console.Write(array[i]);
+}
+System.Console.Write(" ]");
